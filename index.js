90264dd2129a3bde4177e26bb3bef0479469dd89
DWAUUD BELKO <belkodawuud@gmail.com>
2021-10-18 18:44:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+Index is calculated by :
+  - Get current US Big Mac price
+  - Get current Big Mac price from country we're comparing
+  - Divide US Big Mac price by country's Big Mac price
+    (this is called the implied purchasing power)
+  - Get country's currency rate to USD value
+  - Substract currency rate with the implied purchasing power
+  - Divide the previous result by the implied purchasing power
+  - This will give you the Big Mac Index
+  - Make sure you search for updated values!
+
+For example, using figures in July 2008:
+  - the price of a Big Mac was $3.57 in the United States 
+  - the price of a Big Mac was £2.29 in the United Kingdom 
+  - the implied purchasing power parity was $1.56 to £1, that is $3.57/£2.29 = 1.56
+  - this compares with an actual exchange rate of $2.00 to £1 at the time
+  - (2.00 – 1.56) / 1.56 = 28%
+  - the pound was thus overvalued against the dollar by 28%
+*/
+const Usbigmac = 3.57
+
+const Gggigmac = 2.29
+
+const ippgbus = ( Usbigmac/ Gggigmac)
+
+const exruk = 2
+ 
+const exrukindex = ( (exrukindex - ippgbus)/ippgbus )
+
+console. log(exrukindex)
